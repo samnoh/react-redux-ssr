0d13eadde2c6b1c6c7dde1309847cb76093d1ba3
Sam Noh <samnoh21@outlook.com>
2019-11-15 15:27:07 +1300

    chore(): initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Menu from 'components/Menu';
+import RedPage from 'pages/RedPage';
+import BluePage from 'pages/BluePage';
+import 'App.scss';
+
+const App = () => {
+    return (
+        <>
+            <Menu />
+            <hr />
+            <Route path="/red" component={RedPage} />
+            <Route path="/blue" component={BluePage} />
+        </>
+    );
+};
 
 export default App;
